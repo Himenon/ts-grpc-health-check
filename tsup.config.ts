450b54bb73611b470f5fc4c377c6200874f844c1
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-04 22:30:33 +0900

    feat: update esmodule

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  sourcemap: true,
-  clean: true,
-  dts: true,
-  format: ["esm", "cjs"],
+	entry: ["src/index.ts"],
+	minify: false,
+	target: "es2022",
+	format: ["cjs", "esm"],
+	clean: true,
+	dts: true,
+	sourcemap: true,
 });
