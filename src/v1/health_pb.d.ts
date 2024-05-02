583d6ade4418fe9ebdf585a851717fb7bee31b28
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-05-02 14:04:38 +0900

    chore: update

--- src/v1/health_pb.d.ts
+++ src/v1/health_pb.d.ts
@@ -14,9 +14,7 @@ export class HealthCheckRequest extends jspb.Message {
   toObject(includeInstance?: boolean): HealthCheckRequest.AsObject;
   static toObject(includeInstance: boolean, msg: HealthCheckRequest): HealthCheckRequest.AsObject;
   static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
-  static extensionsBinary: {
-    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
-  };
+  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> };
   static serializeBinaryToWriter(message: HealthCheckRequest, writer: jspb.BinaryWriter): void;
   static deserializeBinary(bytes: Uint8Array): HealthCheckRequest;
   static deserializeBinaryFromReader(message: HealthCheckRequest, reader: jspb.BinaryReader): HealthCheckRequest;
@@ -36,9 +34,7 @@ export class HealthCheckResponse extends jspb.Message {
   toObject(includeInstance?: boolean): HealthCheckResponse.AsObject;
   static toObject(includeInstance: boolean, msg: HealthCheckResponse): HealthCheckResponse.AsObject;
   static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
-  static extensionsBinary: {
-    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
-  };
+  static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> };
   static serializeBinaryToWriter(message: HealthCheckResponse, writer: jspb.BinaryWriter): void;
   static deserializeBinary(bytes: Uint8Array): HealthCheckResponse;
   static deserializeBinaryFromReader(message: HealthCheckResponse, reader: jspb.BinaryReader): HealthCheckResponse;
